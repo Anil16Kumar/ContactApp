a911815dc9a515455f62770ad6a43c55d92df4f7
Anil16Kumar <87219848+Anil16Kumar@users.noreply.github.com>
2023-06-30 09:14:06 +0530

    Update index.js

--- contactApp/index.js
+++ contactApp/index.js
@@ -1,22 +1,78 @@
 //Driver Code
 
 
-const User = require("./User");
+const User = require("./user");
 
 try {
-    const admin1 = User.newAdmin("AnilKumar", "ani123")
+    const admin1 = User.newAdmin("Anil", "ani123")
 
-    const user1=admin1.newUser("Anurag","Anu16")
-    const user2=admin1.newUser("Atul","At17")
-    const user3=admin1.newUser("bhanu","bh23")
-    const user4=admin1.newUser("rahul","rah78")
     
-    user1.newContact("wasdd")
+    // CRUD ON ADMIN
 
-     console.log(user1);
-     console.log(user1.contacts[0]);
+    const user1 = admin1.newUser("Sureshk", "sk123")
+    const user2 = admin1.newUser("Rakesh", "r123")
+    const user3 = admin1.newUser("Dineshk", "dk123")
+    
+
+
+    admin1.updateUser("sk123", "name", "Saurav Kumar")
+    admin1.deleteUser("r123")
+
+    // CRUD ON CONTACT
+    
+     user1.newContact("anurag")
+     user1.newContact("bhim")
+     user1.newContact("chhotu")
+     user1.newContact("doli")
+     user1.newContact("eroll")
+
+     user1.updateContact("bhim","kat")
+     user1.deleteContact("chhotu")
+
+     // CRUD ON CONTACT INFO
+
+     user1.addContactInfo("anurag","Home",98989)       
+     user1.addContactInfo("anurag","Telephone",98989)
+     user1.addContactInfo("anurag","Mobile",98989)
+     
+     user1.updateContactInfo("anurag","Mobile",909090)  
+     user1.deleteContactInfo("anurag","Telephone")      
+     
+
+     console.log(admin1.getAllUser());           
+
+     console.log(user1);                           
+     console.log(user1.contacts[0]);               
+
+
+    user2.newContact("Anil")
+    user2.newContact("bhim")
+    user2.newContact("chhavi")
+    user2.newContact("sam")
+    user2.newContact("nik")
+
+    user2.updateContact("bhim","Arjun")
+    user2.deleteContact("chhavi")
+
+     // CRUD ON CONTACT INFO
+
+     user2.addContactInfo("Anil","Home",98989)        
+     user2.addContactInfo("Anil","Telephone",98989)
+     user2.addContactInfo("kola","Mobile",98989)
+     
+     user2.updateContactInfo("Anil","Home",101010)  
+     user2.deleteContactInfo("Anil","Telephone")      
+     
+
+     console.log(admin1.getAllUser());            
+     console.log(user2);                           
+     console.log(user2.contacts[0]);               
+
+
+
 
 
 } catch (error) {
     console.log(error.message)
 }
+
